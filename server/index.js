fa8f8b749c72eff23f1543bb0ef6a9df97fe0b24
Rishikesh K V <rishikeshkaroth@gmail.com>
2023-05-18 01:37:32 +0530

    added / route in backend

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors())
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 // Route Middlewares
 app.use('/api/user', authRoute);
 
