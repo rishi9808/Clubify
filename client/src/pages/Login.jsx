4976346cdb72f14d1bf3a2789d41ddbe61812ff2
Rishikesh K V <rishikeshkaroth@gmail.com>
2023-05-19 01:40:54 +0530

    Created a new Form Component and added this component to the pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,89 +1,46 @@
 import React, { useState } from "react";
+import Forms from "../components/Forms.jsx";
 import { API_URL } from "../const";
 
+const loginInputs = [
+  { name: "email" , type: "email" , label: "Email Id"},
+  { name:"password" , type: "password" , label: "Password"},
+]
+
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    console.log(API_URL);
-
-    const response = await fetch(`${API_URL}/api/user/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
-    // console.log(response)
+  const loginUser = async (formData) => {
     try {
-      var data = await response.json();
+      const response = await fetch(`${API_URL}/api/user/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.status === 200) {
+        const data = await response.json();
+        console.log(data);
+        console.log(data.token);
+        console.log("login successful");
+        alert("Login successful");
+        localStorage.setItem("token", data.token);
+        //window.location.href = '/'
+      } else {
+        console.log("login failed");
+        const error = await response.text();
+        alert(error);
+      }
     } catch (error) {
       console.log(error);
     }
-
-    if (response.status === 200) {
-      console.log(data);
-      console.log(data.token);
-      console.log("login successful");
-      alert("Login successful");
-      localStorage.setItem("token", data.token);
-      //window.location.href = '/'
-    } else {
-      alert(error);
-    }
-    console.log(data);
-  }
+  };
 
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="w-1/3">
-        <h1 className="text-3xl font-bold mb-5">Login</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="email"
-              id="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="password"
-              id="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Login
-            </button>
-          </div>
-        </form>
+        <h1 className="text-xl font-bold mb-5">Login</h1>
+        <Forms inputs={loginInputs} onSubmit={loginUser} />
       </div>
     </div>
   );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,114 +1,76 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Forms } from "../components/Forms.jsx"
 import { API_URL } from "../const";
 
 const registrationInputs = [
   {
     name: "name",
     type: "text",
+    label: "Name"
   },
   {
     name: "email",
     type: "email",
+    label: "Email Id"
   },
   {
     name: "password",
     type: "password",
+    label: "Password"
   },
   {
     name: "regNo",
     type: "text",
+    label: "Register Number"
   },
   {
     name: "branch",
     type: "text",
+    label: "Branch"
   },
   {
     name: "batch",
     type: "number",
+    label: "Batch"
   },
 ];
 
 const Register = () => {
   const navigate = useNavigate();
 
-  const [formData, setFormData] = useState({});
+  const registerUser = async (formData) => {
+    try {
+      const response = await fetch(`${API_URL}/api/user/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
 
-  const handleInputChange = (event) => {
-    const { target } = event;
-    const key = target.name;
-    const value =
-      target.type === "number"
-        ? target.valueAsNumber
-        : target.value;
+      const data = await response.json();
 
-    setFormData((prev) => ({ ...prev, [key]: value }));
-  };
+      if (response.status === 200) {
+        alert("Registration successful");
+        navigate("/login");
+      } else {
+        alert("Registration failed");
+      }
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    console.log(API_URL);
-
-    const response = await fetch(`${API_URL}/api/user/register `, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-
-    const data = await response.json();
-
-    if (response.status === 200) {
-      alert("Registration successful");
-      navigate("/login");
-    } else {
-      alert("Registration failed");
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+      alert("An error occurred during registration");
     }
-
-    console.log(data);
   };
 
   return (
     <div className="container flex justify-center items-center h-screen ">
       <div className="w-1/3">
-      <h1 className="text-3xl font-bold mb-5">Register</h1>
-      <form onSubmit={handleSubmit}>
-        {registrationInputs.map((input, index) => {
-          const value =
-            input.type === "date"
-              ? new Date(formData[input.name]).toISOString().split("T")[0]
-              : formData[input.name];
-
-          return (
-            <div  
-            className="mb-4"
-            >
-            
-            <label 
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor={input.type}>
-              {input.name}
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder={input.name}
-              {...input}
-              required
-              value={value}
-              key={index}
-              onChange={handleInputChange}
-            />
-          </div>
-          );
-        })}
-        <button 
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        type="submit"
-        >Register</button>
-      
-      </form>
-    </div>
+        <h1 className="text-xl font-bold mb-5">Register</h1>
+        <Forms inputs={registrationInputs} onSubmit={registerUser} />
+      </div>
     </div>
   );
 }
