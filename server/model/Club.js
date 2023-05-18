d031bbfb927bc7839ad989d4f7d5acccde128db2
Rishikesh K V <rishikeshkaroth@gmail.com>
2023-05-18 23:42:22 +0530

    enabled timestamps for schemas

--- server/model/Club.js
+++ server/model/Club.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ClubSchema = new mongoose.Schema({
+const ClubSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        unique: true,
-        min: 3,
-        max: 50
+      type: String,
+      required: true,
+      unique: true,
+      min: 3,
+      max: 50,
     },
-    admins: [{ type: mongoose.Schema.Types.ObjectId,
-        ref: 'User' }],
-});
+    admins: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Club", ClubSchema);
--- server/model/User.js
+++ server/model/User.js
@@ -1,42 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 255,
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
     },
     email: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 255,
-        unique: true,
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 1024,
+      type: String,
+      required: true,
+      min: 6,
+      max: 1024,
     },
     regNo: {
-        type: String,
-        required: true,
-        unique: true,
-        min: 6,
+      type: String,
+      required: true,
+      unique: true,
+      min: 6,
     },
     branch: {
-        type: String,
-        required: true,
-        max: 255,
-        min: 3,
+      type: String,
+      required: true,
+      max: 255,
+      min: 3,
     },
     batch: {
-        type: Number,
-        required: true,
-        length: 4,
-    }
-});
+      type: Number,
+      required: true,
+      length: 4,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
